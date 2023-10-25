c97c9bcea2394c14fd5b42e04753fb58f8a9c537
skycode <sikiruyaya1@gmail.com>
2023-10-25 09:42:05 -0700

    Modify the Notifications

--- 0x01-react_intro/task_2/src/Notifications.js
+++ 0x01-react_intro/task_2/src/Notifications.js
@@ -1,10 +1,28 @@
 import React from "react"
 import "./Notifications.css"
+import closeIcon from './Close-icon.png';
+import { getLatestNotification } from './utils';
+
 
 export function Notifications() {
     return (
         <div className="Notifications">
             <p>Here is the list of notifications</p>
+            <ul>
+                <li data-priority="default">New course available</li>
+                <li data-priority="urgent">New resume available</li>
+                <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification()}}></li>
+                <button
+                className='close-icon'
+                aria-label='Close'
+                onClick={() => console.log('Close button has been clicked')}>
+                    <img
+                    src={closeIcon}
+                    alt='Close'
+                    style={{ height: '10px', width: '10px', padding: '10px'}}
+                    />
+                </button>
+            </ul>
         </div>
     )
 }
